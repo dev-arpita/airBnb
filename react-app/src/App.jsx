7aa5c6754bbfe61f797f4018ea7e6c1bf9850567
Arpita Roy <arpita.roy.workar@gmail.com>
2023-03-29 19:38:43 +0530

    Project setup: Hero

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import NavBar from './components/NavBar'
+import Hero from './components/Hero'
 import './App.css'
 
 function App() {
   return (
-    <NavBar />
+    <div className='container'>
+      <NavBar />
+      <Hero />
+    </div>
   )
 }
 
