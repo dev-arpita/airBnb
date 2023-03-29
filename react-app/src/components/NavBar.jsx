aa6f658edce79e9ebefdd3f9b64c1bc68dfb64af
Arpita Roy <arpita.roy.workar@gmail.com>
2023-03-29 18:53:24 +0530

    Project setup: Navbar

---
+++ react-app/src/components/NavBar.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function NavBar() {
+
+    return (
+        <nav>
+            <img src="./images/airbnb-logo.png" className="logo"/>
+        </nav>
+    )
+}
