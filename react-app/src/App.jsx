f962a0012160723f12aff9e62202bfea55156414
Arpita Roy <arpita.roy.workar@gmail.com>
2023-03-31 18:04:46 +0530

    Project: Card Component

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import NavBar from './components/NavBar'
 import Hero from './components/Hero'
+import Card from './components/Card'
 import './App.css'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className='container'>
       <NavBar />
       <Hero />
+      <Card />
     </div>
   )
 }
