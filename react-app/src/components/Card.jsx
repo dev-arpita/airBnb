0f08c6ca697f93d5a0c0b631a3e135a366e8f04e
Arpita Roy <arpita.roy.workar@gmail.com>
2023-04-02 20:24:53 +0530

    Destructuring props

--- react-app/src/components/Card.jsx
+++ react-app/src/components/Card.jsx
@@ -1,17 +1,18 @@
 import React from "react"
 
 export default function Card(props) {
+    const{ img, ratingScore, ratingCount, location, text, cost } = props
     return (
         <div className="card">
-            <img src={props.img} className="card--image" />
+            <img src={img} className="card--image" />
             <div className="card--stats">
                 <img src="../images/star.png" className="card--star" />
-                <span>{props.ratingScore}</span>
-                <span className="gray">{props.ratingCount} </span>
-                <span className="gray">{props.location}</span>
+                <span>{ratingScore}</span>
+                <span className="gray">{ratingCount} </span>
+                <span className="gray">{location}</span>
             </div>
-            <p>{props.text}</p>
-            <p><span className="bold">{props.cost}</span> / person</p>
+            <p>{text}</p>
+            <p><span className="bold">{cost}</span> / person</p>
         </div>
     )
 }
