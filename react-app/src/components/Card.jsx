ba8f0f397a5cc05e85511026efe22976e770ea54
Arpita Roy <arpita.roy.workar@gmail.com>
2024-04-28 21:12:05 +0530

    Update: A11Y Audit

--- react-app/src/components/Card.jsx
+++ react-app/src/components/Card.jsx
@@ -12,9 +12,9 @@ export default function Card(props) {
     return (
         <div className="card">
             {badgeText && <div className="card--badge">{badgeText}</div>}
-            <img src={`../images/${props.coverImg}`} className="card--image" />
+            <img src={`../images/${props.coverImg}`} className="card--image" alt="Main card image." />
             <div className="card--stats">
-                <img src="../images/star.png" className="card--star" />
+                <img src="../images/star.png" className="card--star" alt="star icon."/>
                 <span>{props.stats.rating}</span>
                 <span className="gray">({props.stats.reviewCount}) • </span>
                 <span className="gray">{props.location}</span>
